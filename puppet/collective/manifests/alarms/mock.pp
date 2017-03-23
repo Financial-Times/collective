af444a8576ac8874d4d5e272aba066f14139c124
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-03-23 09:48:55 +0000

    Add mock.pp for syntax testing

---
+++ puppet/collective/manifests/alarms/mock.pp
@@ -0,0 +1,22 @@
+class collective::alarms::mock (
+  $config_file = false
+) {
+
+  notify { "class-name-notify":
+    message => "Class name ${name}" #name variable references the class name, eg. collective::alarms::mock
+  }
+
+  if $config_file {
+    $config_file_path = $config_file
+  }
+  else {
+    $subclass_name = split($name, '::')[-1]
+    $config_file_path = "config/${subclass_name}.yml"
+  }
+
+  notify { "config_file-notify":
+    message => "Using config file ${config_file_path}" #references the subclass name, eg. mock
+  }
+
+
+}
